94f994f7ac4588276c304caf76e32641f28f5371
heimdalalr <lamorliere@gmail.com>
2021-10-31 00:34:50 -0400

    parts 1-6 completed

---
+++ profile.js
@@ -0,0 +1,11 @@
+function favColor() {
+    alert("My favorite color is pink!");
+  }
+
+  function favPlace(){
+      alert("My favorite place is my bed with freshly cleaned sheets.")
+  }
+
+  function favRitual(){
+      alert("My favorite ritual is studying languages on Duolingo daily to increase my learning streak.")
+  }
