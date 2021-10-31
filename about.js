937b824bc07686a7fd3cff896b178c0b91b84002
heimdalalr <lamorliere@gmail.com>
2021-10-31 00:03:31 -0400

    profile.js added

--- about.js
+++ about.js
@@ -5,10 +5,25 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	// console.log('form submit');
+	formSuccess();
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function formSuccess() {
+	alert("form has been submitted successful!");
+  }
+
+// let ducky = document.getElementById="ducky"
+
+// ducky.addEventListener("mouseover", function(event){
+// 	alert("You are very clever!");
+// })
+
+function Alert(e){
+	alert("You are very clever!");
+   };
